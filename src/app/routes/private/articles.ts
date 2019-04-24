59c473b387bbb27256bd47b7f2cbb25493899bd3
DevilTea <ch870814@gmail.com>
2019-04-25 00:10:33 +0800

    Add controllers to routes

--- src/app/routes/private/articles.ts
+++ src/app/routes/private/articles.ts
@@ -1,12 +1,13 @@
 import Router from 'koa-router'
 
 import * as users from '@controllers/users'
+import * as articles from '@controllers/articles'
 
 const router: Router = new Router()
 
-router.post('/', users.isAdmin)
-router.patch('/:articleId', users.isAdmin)
-router.delete('/:articleId', users.isAdmin)
+router.post('/', users.isAdmin, articles.createArticle)
+router.patch('/:articleId', users.isAdmin, articles.updateArticle)
+router.delete('/:articleId', users.isAdmin, articles.deleteArticle)
 router.post('/:articleId/comments', users.isMember)
 router.patch('/:articleId/comments/:commentId', users.isMember)
 router.delete('/:articleId/comments/:commentId', users.isMember)
--- src/app/routes/public/articles.ts
+++ src/app/routes/public/articles.ts
@@ -1,10 +1,12 @@
 import Router from 'koa-router'
+import * as articles from '@controllers/articles'
 
 const router: Router = new Router()
 
-router.get('/')
-router.get('/:articleId')
-router.post('/:articleId/views/count')
+router.get('/', articles.getArticleList)
+router.get('/:articleId/info', articles.getArticleInfo)
+router.post('/:articleId/content', articles.getArticleContent)
+router.post('/:articleId/views/count', articles.increaseViewCount)
 router.get('/:articleId/comments')
 router.get('/:articleId/comments/count')
 router.get('/:articleId/comments/:commentId')
