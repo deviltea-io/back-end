7959e25579b3233598cf24d1d5931e56f1bef6d9
DevilTea <ch870814@gmail.com>
2019-04-25 00:10:07 +0800

    Update article model

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,4 +1,5 @@
-import { model, Model, Document, Schema } from 'mongoose'
+import { model, Model, Document, Schema, Types } from 'mongoose'
+import { commentSchema, CommentDocument } from './comment'
 
 export interface ArticleProperties {
   coverUrl: string
@@ -6,8 +7,13 @@ export interface ArticleProperties {
   tags: string[]
   content: string
   viewCount: number
+  commentCount: number
   createdTime: Date
   updatedTime?: Date
+  deleted: boolean
+  password: string
+  comments: Types.DocumentArray<CommentDocument>
+  locked: boolean
 }
 export interface ArticleMethods {}
 export interface ArticleDocument
@@ -17,36 +23,65 @@ export interface ArticleDocument
 export interface ArticleStatics {}
 export interface ArticleModel extends ArticleStatics, Model<ArticleDocument> {}
 
-const articleSchema: Schema = new Schema({
-  coverUrl: {
-    type: String,
-    required: true
+const articleSchema: Schema = new Schema(
+  {
+    coverUrl: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    tags: {
+      type: [String],
+      required: true,
+      default: (): string[] => ['未分類']
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    viewCount: {
+      type: Number,
+      required: true,
+      default: (): number => 0
+    },
+    updatedTime: {
+      type: Date,
+      required: false
+    },
+    deleted: {
+      type: Boolean,
+      required: true,
+      default: () => false
+    },
+    password: {
+      type: String,
+      required: false,
+      default: () => ''
+    },
+    comments: {
+      type: [commentSchema],
+      required: true,
+      default: (): Types.DocumentArray<CommentDocument> =>
+        new Types.DocumentArray<CommentDocument>()
+    }
   },
-  title: {
-    type: String,
-    required: true
-  },
-  tags: {
-    type: [String],
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  viewCount: {
-    type: Number,
-    required: true,
-    default: (): number => 0
-  },
-  updatedTime: {
-    type: Date,
-    required: false
-  }
-})
+  { capped: true }
+)
 articleSchema.virtual('createdTime').get(function (this: ArticleDocument): Date {
   return this._id.getTimestamp()
 })
+articleSchema.virtual('locked').get(function (this: ArticleDocument): boolean {
+  return !!this.password
+})
+articleSchema
+  .virtual('commentCount')
+  .get(function (this: ArticleDocument): number {
+    return this.comments.length
+  })
+
 export const Article: ArticleModel = model<ArticleDocument, ArticleModel>(
   'Article',
   articleSchema
